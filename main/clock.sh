0df99cf515d5972974c2771238b3f0ac96a1e16a
barrett <66580279+onkoe@users.noreply.github.com>
2021-08-18 19:46:01 -0500

    change clock.sh
    to make it look like i have eyes

--- main/clock.sh
+++ main/clock.sh
@@ -4,5 +4,5 @@
 
 gsettings set org.gnome.desktop.interface clock-format '12h'
 gsettings set org.gnome.desktop.interface clock-show-date 'false'
-gsettings set org.gnome.desktop.interface clock-seconds 'true'
+gsettings set org.gnome.desktop.interface clock-show-seconds 'true'
 gsettings set org.gnome.desktop.interface enable-animations 'true'
