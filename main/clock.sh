3ed5c864b3e4ecdc486216be8d4067cc506ca965
barrett <66580279+onkoe@users.noreply.github.com>
2021-08-18 19:44:06 -0500

    Create clock.sh

---
+++ main/clock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 
+
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.interface clock-show-date 'false'
+gsettings set org.gnome.desktop.interface clock-seconds 'true'
+gsettings set org.gnome.desktop.interface enable-animations 'true'
